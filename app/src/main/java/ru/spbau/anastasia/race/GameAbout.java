8c9f5874a3f8b3110093130945ccfda82ca2e7c7
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2015-10-08 11:29:57 +0300

    Activity GameAbout

--- app/src/main/java/ru/spbau/anastasia/race/GameAbout.java
+++ app/src/main/java/ru/spbau/anastasia/race/GameAbout.java
@@ -4,6 +4,7 @@ import ru.spbau.anastasia.race.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
