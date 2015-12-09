3c2a4386e0dbdd13b99a44073a25eedbe7510c2d
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2015-12-09 17:49:03 +0300

    Cosmetic changes

--- app/src/main/java/ru/spbau/anastasia/race/GameAbout.java
+++ app/src/main/java/ru/spbau/anastasia/race/GameAbout.java
@@ -3,17 +3,23 @@ package ru.spbau.anastasia.race;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class GameAbout extends Activity {
 
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_game_about);
+
+        t = (TextView) findViewById(R.id.gameAbout);
+        t.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void onClickButtonBackGameAbout(View view) {
--- app/src/main/java/ru/spbau/anastasia/race/GameInfo.java
+++ app/src/main/java/ru/spbau/anastasia/race/GameInfo.java
@@ -8,22 +8,29 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.widget.TextView;
 
 
 public class GameInfo extends Activity {
 
+    TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_game_info);
+
+        t = (TextView) findViewById(R.id.textView);
+        t.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void onClickButtonBackGameInfo(View view) {
         finish();
     }
+
 }
