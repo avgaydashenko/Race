3b7a4e1b8eaff5aa385e8a4ae246f2665cc1d546
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2015-12-14 16:03:06 +0300

    GameMenu Buttons

--- app/src/main/java/ru/spbau/anastasia/race/OnePlayerOption.java
+++ app/src/main/java/ru/spbau/anastasia/race/OnePlayerOption.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 
 public class OnePlayerOption extends Activity {
 
